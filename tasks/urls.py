b7e50f785e6cf887014997e4bb9e188cc6ee2b0d
Juan Manuel Burgues Schaab <jschaab@sea.corp.expecn.com>
2021-11-21 09:11:31 -0300

    Adds named URLs for linking to a task detail on tasks_list.html

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
+from django.conf.urls import url
 from . import views
 
+app_name = 'tasks'
+
 urlpatterns = [
-    path('', views.task_list)
+    url(r'^$', views.task_list),
+    # (?P<task_id>[0-9]+) format (?P<VARIABLE>REGEX) specifies the regex for that variable and that will be passed to
+    # the view fun. For name='detail' is a name given to reference the URL created dynamically (used on tasks_list.html)
+    url(r'^(?P<task_id>[0-9]+)/$', views.task_details, name='detail'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from .models import Task
+from django.http import HttpResponse
 
 
 def task_list(request):
-    tasks = Task.objects.all()
+    tasks = Task.objects.all()  # we can append here .order_by('date') or any other.
     return render(request, 'tasks/tasks_list.html', {'tasks': tasks})
+
+
+def task_details(request, task_id):
+    task = Task.objects.get(id=task_id)
+    # Render a template and send data to it.
+    # Pass the variable task created to the template with {'task': task}
+    return render(request, 'tasks/task_detail.html', {'task': task})
