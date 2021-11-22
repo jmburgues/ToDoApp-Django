49df9c975ebb35c04d7cde51d612541b0a8c29e1
Juan Manuel Burgues Schaab <jschaab@sea.corp.expecn.com>
2021-11-22 18:40:51 -0300

    Adds update task feature

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.urls import path
 from . import views
 
 app_name = 'tasks'
@@ -7,6 +8,9 @@ urlpatterns = [
     url(r'^$', views.task_list, name='list'),
     url(r'^json$', views.task_to_json, name='task_list'),
     url(r'^create/$', views.task_create, name='create'),
+    url(r'^(?P<task_id>[0-9]+)/state/$', views.task_change_state, name='change_state'),
+    url(r'^(?P<task_id>[0-9]+)/delete/$', views.task_delete, name='delete'),
+    url(r'^(?P<task_id>[0-9]+)/update/$', views.task_update, name='update'),
     # (?P<task_id>[0-9]+) format (?P<VARIABLE>REGEX) specifies the regex for that variable and that will be passed to
     # the view fun. For name='detail' is a name given to reference the URL created dynamically (used on tasks_list.html)
     url(r'^(?P<task_id>[0-9]+)/$', views.task_details, name='detail'),
--- tasks/views.py
+++ tasks/views.py
@@ -1,12 +1,14 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
-from django.core import serializers
+from django.core import serializers as deserialize
 from .models import Task
 from django.contrib.auth.models import User
 from . import forms
 import json
 from rest_framework import serializers
+from django.views.generic.edit import UpdateView
+from django.urls import reverse_lazy
 
 
 # Necessary classes to serialize nested objects
@@ -25,7 +27,7 @@ class TaskSerializer(serializers.ModelSerializer):
 
 
 def task_list(request):
-    tasks = Task.objects.all()  # we can append here .order_by('date') or any other.
+    tasks = Task.objects.all().order_by('date')  # we can append here .order_by('date') or any other.
     return render(request, 'tasks/tasks_list.html', {'tasks': tasks})
 
 
@@ -41,6 +43,47 @@ def task_details(request, task_id):
     # Render a template and send data to it.
     # Pass the variable task created to the template with {'task': task}
     return render(request, 'tasks/task_detail.html', {'task': task})
+
+
+@login_required(login_url="/accounts/login/")
+def task_change_state(request, task_id):
+    task = Task.objects.get(id=task_id)
+    if task.state == task.IN_PROGRESS:
+        task.state = task.FINISHED
+    else:
+        task.state = task.IN_PROGRESS
+    task.save()
+    return redirect('tasks:list')
+
+
+@login_required(login_url="/accounts/login/")
+def task_delete(request, task_id):
+    task = Task.objects.get(id=task_id)
+    task.delete()
+    return redirect('tasks:list')
+
+
+@login_required(login_url="/accounts/login/")
+def task_update(request, task_id):
+    if request.method == 'POST':
+        task = Task.objects.get(id=task_id)
+        form = forms.CreateTask(request.POST, instance=task)  # creates a form with data coming from the request
+        if form.is_valid():
+            obj = form.save(commit=False)
+            userIds = request.POST.getlist('assigned')
+            obj.assigned.set(userIds)
+            obj.save()
+            return redirect('tasks:list')
+    else:
+        task = Task.objects.get(id=task_id)
+        form = forms.CreateTask(initial={
+            'id': task_id,
+            'title': task.title,
+            'body': task.body,
+            'state': task.state,
+            'assigned': task.assigned.all()
+        })  # creates a form object from tasks/forms.py
+    return render(request, 'tasks/task_update.html', {'form': form, 'task': task})
 
 
 # login decorator prevents unauthenticated users
